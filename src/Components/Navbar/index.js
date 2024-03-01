ce01bb74ce547bad1448e7f30e2a5b85c592a40e
PremkumarAlkunte <premkumar7official@gmail.com>
2024-03-01 21:04:53 +0530

    deployment changes

--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -22,11 +22,11 @@ const Navbar = () => {
           }} />
         </MobileIcon>
         <NavItems>
-          <NavLink href="#about">About</NavLink>
-          <NavLink href='#skills'>Skills</NavLink>
-          <NavLink href='#experience'>Experience</NavLink>
-          <NavLink href='#projects'>Projects</NavLink>
-          <NavLink href='#education'>Education</NavLink>
+          <NavLink href="/#about">About</NavLink>
+          <NavLink href='/#skills'>Skills</NavLink>
+          <NavLink href='/#experience'>Experience</NavLink>
+          <NavLink href='/#projects'>Projects</NavLink>
+          <NavLink href='/#education'>Education</NavLink>
         </NavItems>
         <ButtonContainer>
           <GitHubButton href={Bio.github} target="_blank">Github Profile</GitHubButton>
@@ -34,19 +34,19 @@ const Navbar = () => {
         {
           isOpen &&
           <MobileMenu isOpen={isOpen}>
-            <MobileLink href="#about" onClick={() => {
+            <MobileLink href="/#about" onClick={() => {
               setIsOpen(!isOpen)
             }}>About</MobileLink>
-            <MobileLink href='#skills' onClick={() => {
+            <MobileLink href='/#skills' onClick={() => {
               setIsOpen(!isOpen)
             }}>Skills</MobileLink>
-            <MobileLink href='#experience' onClick={() => {
+            <MobileLink href='/#experience' onClick={() => {
               setIsOpen(!isOpen)
             }}>Experience</MobileLink>
-            <MobileLink href='#projects' onClick={() => {
+            <MobileLink href='/#projects' onClick={() => {
               setIsOpen(!isOpen)
             }}>Projects</MobileLink>
-            <MobileLink href='#education' onClick={() => {
+            <MobileLink href='/#education' onClick={() => {
               setIsOpen(!isOpen)
             }}>Education</MobileLink>
             <GitHubButton style={{padding: '10px 16px',background: `${theme.primary}`, color: 'white',width: 'max-content'}} href={Bio.github} target="_blank">Github Profile</GitHubButton>
