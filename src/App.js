d1f832439ad3d258b3c7daaac12ae55700e9cdc5
PremkumarAlkunte <premkumar7official@gmail.com>
2024-03-01 20:10:35 +0530

    modified commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import { ThemeProvider } from "styled-components";
 import { useState, useEffect } from "react";
-// import { darkTheme, lightTheme } from './Utils/themes'
-import Navbar from "./components/Navbar";
+import { darkTheme, lightTheme } from './Utils/themes'
+import Navbar from "./Components/Navbar/index";
 import './App.css';
 import { BrowserRouter as Router } from 'react-router-dom';
-// import HeroSection from "./components/HeroSection";
-// import Skills from "./components/Skills";
-// import Projects from "./components/Projects";
-// import Contact from "./components/Contact";
-// import Footer from "./components/Footer";
-// import Experience from "./components/Experience";
-// import Education from "./components/Education";
-// import ProjectDetails from "./components/ProjectDetails";
-// import styled from "styled-components";
+import HeroSection from "./Components/HeroSection/index";
+import Skills from "./Components/Skills/index";
+import Projects from "./Components/Projects/index";
+import Contact from "./Components/Contact/index";
+import Footer from "./Components/Footer/index";
+import Experience from "./Components/Experience/index";
+import Education from "./Components/Education/index";
+import ProjectDetails from "./Components/ProjectDetails/index";
+import styled from "styled-components/";
 
 const Body = styled.div`
   background-color: ${({ theme }) => theme.bg};
